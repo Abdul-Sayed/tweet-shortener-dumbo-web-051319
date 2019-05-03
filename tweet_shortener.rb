57410f7f86ab9313728ed5f4ef8fd6c5df45c357
Abdul <awsayed@alaska.edu>
2019-05-03 03:11:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def word_substituter(tweet_string, dictionary)
+
+  # convert tweet string to array with .split
+  tweet_array = tweet_string.split(" ")
+
+  # compare words in keys array to tweet array
+  for key in dictionary.keys
+    tweet_array.map { |word|
+      # replace matching words in tweet_array with the key's value from the dictionary.
+      if (word == key)
+        word.replace dictionary[key]
+      end
+    }
+  end
+
+  #convert the modified tweet array to string with .join and return it
+  return tweet_array.join(" ")
+end
